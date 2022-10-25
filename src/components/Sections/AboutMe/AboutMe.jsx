d5e5d6efc1f9cd86c151ed4f52056611e283e631
Carles Galino <cgalino@promo-soft.eu>
2022-10-25 13:02:04 +0200

    l

--- src/components/Sections/AboutMe/AboutMe.jsx
+++ src/components/Sections/AboutMe/AboutMe.jsx
@@ -7,6 +7,25 @@ function AboutMe() {
                     <span className="secondary">1.</span>
                     <h2>Conóceme!</h2>
                 </div>
+                <div id="about_main" className="flex flex-row items-center gap-8">
+                    <div id="about_sidebar">
+                        <div id="about_foto">foto</div>
+                        <div id="about_links">links</div>
+                    </div>
+                    <div id="about_text">
+                        <div id="about_card" className="card">
+                            <h3>¡Bienvenido!</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                            </p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
