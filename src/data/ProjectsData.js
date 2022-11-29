b07004021f94463dfcd6e2f83c5d54b607dfe128
Carles Galino <cgalino@promo-soft.eu>
2022-11-29 16:06:09 +0100

    l

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,7 +4,9 @@ const data = [
         name: 'GUC',
         title: 'Gestión unificada de contactos',
         web: 'https://www.promo-soft.eu/products/gucweb.html',
-        body: '',
+        body: '<p>Software <b>CTI</b> diseñado para la automatización y gestión eficiente del trabajo de un Contact-Center</p><p>Comunicación a través de múltiples canales(Telefonía, chat, whatsapp, videollamada, email)</p> ',
+        techs: [2, 3, 6],
+        link: null,
         images: [
             'img/projects/GUC/1.png',
             'img/projects/GUC/2.png',
@@ -14,14 +16,44 @@ const data = [
     },
     {
         id: 2,
-        name: 'Agent Lite',
-        title: 'Guc',
-        htmlBody: '<p className="bold">Gestión unificada de contactos<p>',
+        name: 'VoIP',
+        title: 'Operador IP',
+        body: '<p>Portal de gestión para clientes de telefonía IP de la red de Promo-soft. Generación y descarga de facturas, datos estadísticos y historicos de llamadas, envio de faxes.<p>',
+        techs: [2, 3, 6, 7],
+        link: null,
         images: [
-            'img/projects/AGENLITE/1.png',
-            'img/projects/AGENLITE/2.png',
-            'img/projects/AGENLITE/3.png',
-            'img/projects/AGENLITE/4.png',
+            'img/projects/VOIP/1.png',
+            'img/projects/VOIP/2.png',
+            'img/projects/VOIP/3.png',
+            'img/projects/VOIP/4.png',
+        ]
+    },
+    {
+        id: 3,
+        name: 'Contacto Web Promo-soft',
+        link: 'https://www.promo-soft.eu/contacto.html',
+        title: 'Contacto en linea desde la web a través de chat, whatsapp, videollamada',
+        body: '<p><p>',
+        techs: [2, 3, 6, 7],
+        images: [
+            'img/projects/WEBPROMO/1.png',
+            'img/projects/WEBPROMO/2.png',
+            'img/projects/WEBPROMO/3.png',
+            'img/projects/WEBPROMO/4.png',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Sorarers',
+        link: 'https://www.sorarers.com',
+        title: 'Proyecto personal en desarrollo',
+        body: '<p>App con noticias sobre Sorare (Juego de coleccionables de futbol NFT).</p><p>Proyecto personal en desarrollo</p>',
+        techs: [1, 3, 7],
+        images: [
+            'img/projects/SORARERS/1.png',
+            'img/projects/SORARERS/2.png',
+            'img/projects/SORARERS/3.png',
+            'img/projects/SORARERS/4.png',
         ]
     },
 ];
